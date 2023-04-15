fdc1a3d0428b6fba539ee4ec43a8a63149c056d8
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-04-15 18:28:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+// Get all the bold tags in the paragraph
+let boldTags = document.getElementsByTagName('strong');
+
+// Define the highlight function
 function highlight() {
-    //Write your code here
-
-
+  // Loop through all the bold tags and change their color to green
+  for (let i = 0; i < boldTags.length; i++) {
+    boldTags[i].style.color = 'green';
+  }
 }
 
-
+// Define the return_normal function
 function return_normal() {
-    //Write your code here
-
-    
+  // Loop through all the bold tags and change their color back to black
+  for (let i = 0; i < boldTags.length; i++) {
+    boldTags[i].style.color = 'black';
+  }
 }
